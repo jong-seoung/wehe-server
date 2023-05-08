37cf3e8d9770956fa18f29fa2895e2d9dc233f33
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-05-08 17:26:20 +0900

    Change the url form to /api/v1/...

--- weheproject/urls.py
+++ weheproject/urls.py
@@ -38,5 +38,5 @@ urlpatterns = [
    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
    path('admin/', admin.site.urls),
-   path('user/', include('user.urls')),
+   path('api/v1/user/', include('user.urls')),
 ]
