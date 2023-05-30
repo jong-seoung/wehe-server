c88424993b521cff49370f7973bddc1cf0d04e71
백종성 <jongseoung@192.168.nate.com>
2023-05-30 10:49:27 +0900

    test: user모델의 career관련 내용 삭제

--- tests/user/test_models.py
+++ tests/user/test_models.py
@@ -19,8 +19,6 @@ class UserModelTest(TestCase):
         self.assertEqual(self.user.name, None)
         self.assertIsNotNone(self.user.nickname)
         self.assertEqual(self.user.birthday, None)
-        self.assertEqual(self.user.job, None)
-        self.assertEqual(self.user.career, None)
 
         self.assertFalse(self.user.is_staff)
         self.assertFalse(self.user.is_superuser)
@@ -40,8 +38,6 @@ class UserFirstLogin(TestCase):
             name='test',
             nickname='nick name',
             birthday='2000-12-11',
-            job='BACKEND',
-            career='취준생'
         )
 
     def test_name_label(self):
@@ -61,23 +57,3 @@ class UserFirstLogin(TestCase):
         value = self.user.birthday
 
         self.assertRegex(value, expected_format)
-
-    def test_job_label(self):
-        valid_jobs = ['PRONTEND', 'BACKEND', 'DESIGN']
-        value = self.user.job
-
-        self.assertIn(value, valid_jobs)
-
-        max_length = self.user._meta.get_field("job").max_length
-        value_length = len(self.user.job)
-
-        self.assertLessEqual(value_length, max_length)
-
-    def test_career_label(self):
-        n = 100  # 총 경력 레벨의 수
-        valid_careers = [f"{i}년차" for i in range(1, n + 1)] + ['취준생']
-
-        career_value = self.user.career
-
-        self.assertIn(career_value, valid_careers)
-
