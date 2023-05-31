e30a3e05eda971bcd687267f7704ee237f40ca1f
백종성 <jongseoung@192.168.nate.com>
2023-05-31 09:26:56 +0900

    refactor: url패턴 변경
    swagger url 패턴이 django 2.0 이상의 버전에서의 정의에 맞지않는 부분을 수정

--- weheproject/urls.py
+++ weheproject/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [
-        path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        path('swagger.<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     ]
