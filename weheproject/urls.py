f9f4da958c8e8a453399f8b0919235f83b71529a
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-07-01 23:17:14 +0900

    fix: 깃허브 회원가입 에러 해결
    에러 내용: Reverse for 'account_confirm_email' not found. 'account_confirm_email' is not a valid view function or pattern name.

--- weheproject/urls.py
+++ weheproject/urls.py
@@ -1,12 +1,26 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from dj_rest_auth.registration.views import VerifyEmailView
 
 from weheproject.swagger import get_swagger_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/user/", include("user.urls")),
+
+    path("signup/", include("dj_rest_auth.registration.urls")),
+    path("verify-email/", VerifyEmailView.as_view(), name="rest_verify_email"),
+    path(
+        "account-confirm-email/",
+        VerifyEmailView.as_view(),
+        name="account_confirm_email_sent",
+    ),
+    path(
+        "account-confirm-email/<key>/",
+        VerifyEmailView.as_view(),
+        name="account_confirm_email",
+    ),
 ]
 
 if settings.DEBUG:
