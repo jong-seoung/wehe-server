d2a1eab68f34dbedd194ae64ca982fae9c37e41b
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-07-15 00:52:24 +0900

    feat: postgresql로 변경

--- weheproject/settings/dev.py
+++ weheproject/settings/dev.py
@@ -59,8 +59,12 @@ CORS_ALLOW_HEADERS = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": get_env_variable("DB_NAME"),
+        "USER": get_env_variable("DB_USER"),
+        "PASSWORD": get_env_variable("DB_PASSWORD"),
+        "HOST": get_env_variable("DB_HOST"),
+        "PORT": get_env_variable("DB_PORT"),
     }
 }
