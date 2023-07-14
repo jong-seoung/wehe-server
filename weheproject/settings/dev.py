5f4d7b41dd21b686b7a88a2d9203a98f6b414f97
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-07-14 21:44:15 +0900

    feat: 배포를 위해서 settings파일 정리

--- weheproject/settings/dev.py
+++ weheproject/settings/dev.py
@@ -1,10 +1,38 @@
 from .base import *
 
+def get_env_variable(var_name):
+  try:
+    return os.environ[var_name]
+  except KeyError:
+    error_msg = 'Set the {} environment variable'.format(var_name)
+    raise ImproperlyConfigured(error_msg)
+
 ALLOWED_HOSTS = ["*"]
 
-DEBUG = True
+SECRET_KEY = get_env_variable('DJANGO_SECRET')
+STATE = get_env_variable("STATE")
+KAKAO_REST_API_KEY = get_env_variable("KAKAO_REST_API_KEY")
+SOCIAL_AUTH_GITHUB_CLIENT_ID = get_env_variable("SOCIAL_AUTH_GITHUB_CLIENT_ID")
+SOCIAL_AUTH_GITHUB_SECRET = get_env_variable("SOCIAL_AUTH_GITHUB_SECRET")
+SOCIAL_AUTH_GOOGLE_CLIENT_ID = get_env_variable("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
+SOCIAL_AUTH_GOOGLE_SECRET = get_env_variable("SOCIAL_AUTH_GOOGLE_SECRET")
+
+DEBUG = False
+
+SIMPLE_JWT = {
+    "JWT_SECRET_KEY": SECRET_KEY,
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "TOKEN_OBTAIN_SERIALIZER": "user.serializers.MyTokenObtainPairSerializer",
+}
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_COOKIE_NAME = 'XSRF-TOKEN'
+CSRF_HEADER_NAME = 'X-XSRF-TOKEN'
 
 CORS_ALLOW_METHODS = [
     "GET",
@@ -27,10 +55,13 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
 ]
 
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": 'django.db.backends.postgresql',
+        'NAME': get_env_variable('DB_NAME'),
+        'USER': get_env_variable('DB_USER'),
+        'PASSWORD': get_env_variable('DB_PASSWORD'),
+        'HOST': get_env_variable('DB_HOST'),
+        'PORT': get_env_variable('DB_PORT'),
     }
 }
