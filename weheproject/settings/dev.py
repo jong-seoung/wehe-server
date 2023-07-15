9d3322e8802f3ae73808428c4028da800473eb28
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-07-16 01:00:53 +0900

    fix: cors에러

--- weheproject/settings/dev.py
+++ weheproject/settings/dev.py
@@ -44,24 +44,3 @@ SIMPLE_JWT = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS",
-]
-
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
