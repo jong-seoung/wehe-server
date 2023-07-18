73d6bc20aad5bddf39f3214e4f983bda50ca6666
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-07-18 21:05:13 +0900

    feat: create apps(comments,posts)

--- weheproject/settings/base.py
+++ weheproject/settings/base.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "user",
     "core",
     "skills",
+    "comments",
+    "posts",
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
