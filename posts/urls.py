4014c9bbcc7eccfe5660a15738f1f3a192222901
백종성 <jongseoung@192.168.nate.com>
2023-07-26 09:40:00 +0900

    feat: Post URL 설정

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
-    path("post/", views.PostAPI.as_view(), name="post-list"),
-    path("post/<int:pk>/", views.PostDetailAPI.as_view(), name="post-detail"),
-    path("post/<int:pk>/like/", views.PostLikeAPI.as_view(), name="post-like"),
+    path("", views.PostAPI.as_view(), name="post-list"),
+    path("<int:pk>/", views.PostDetailAPI.as_view(), name="post-detail"),
+    path("<int:pk>/like/", views.PostLikeAPI.as_view(), name="post-like"),
 ]
