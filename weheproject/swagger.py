a35e3c0c7acc226ad4e61d3683ba28ab9f041a0a
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-08-13 01:07:04 +0900

    feat: swagger 설명 및 https 스키마 추가

--- weheproject/swagger.py
+++ weheproject/swagger.py
@@ -2,6 +2,14 @@ from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["https", "http"]
+        return schema
 
 
 def get_swagger_urls():
@@ -9,7 +17,8 @@ def get_swagger_urls():
         openapi.Info(
             title="weheproject API",
             default_version="v1",
-            description="API for weheproject",
+            description="Wehe API 문서입니다.\n토큰 인증을 하실 때는 헤더에 'Token xxx' 형태로 액세스 토큰에 Token(Bearer) 접두사를 붙여주세요."
+                        "\n로컬에서 개발하실 때에는 하단의 HTTP 스키마를 선택해 주시고, 실제 서버에서는 HTTPS 스키마를 선택해 주세요.",
             terms_of_service="https://github.com/orgs/Team-We-Here/repositories",
         ),
         validators=["flex"],
