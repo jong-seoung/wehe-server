5a83f66a63f895bf6713dccf66cd41a2dec5e33e
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-08-28 14:35:08 +0900

    feat: skills 인스턴스 변경

--- skills/models.py
+++ skills/models.py
@@ -5,3 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class Skill(models.Model):
     name = models.CharField(_("skill"), max_length=30, blank=True, null=True)
     image = models.ImageField(_("skill image"), upload_to="skill_images/", blank=True)
+
+    def __str__(self):
+        return self.name
