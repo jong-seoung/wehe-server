c148bdf163fc3a3633e4a063bc434a96c8ec9706
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-09-01 22:02:55 +0900

    chore

--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("auth", "0012_alter_user_first_name_max_length"),
-        ("roles", "__first__"),
+        ("roles", "0001_initial"),
         ("skills", "__first__"),
     ]
 
